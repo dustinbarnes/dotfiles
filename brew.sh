38d43035e6f8fc2076d1c94552ae82f5d8158ffb
Dustin Barnes <dustin.barnes@gmail.com>
2023-03-17 13:35:19 -0700

    Update brew.sh
    More minimalism. CLI install of gui apps isn't all that time-saving.

--- brew.sh
+++ brew.sh
@@ -12,7 +12,7 @@ brew upgrade
 
 brew install bash bash-completion2 coreutils grep openssh openssl screen \
      jq yq pcre gettext git git-extras git-lfs p7zip tree wget unrar xz \
-     curl htop httpie nodejs svn
+     curl
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
@@ -22,10 +22,6 @@ fi;
 
 # Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
-
-# GUI Apps
-brew cask install clipy caffeine bettertouchtool slack \
-    docker iterm2 whatsapp obsidian postman vlc
 
 # Remove outdated versions from the cellar.
 brew cleanup
@@ -37,10 +33,3 @@ if ! [ -x "$(command -v sdk)" ]; then
   curl -s "https://get.sdkman.io" | bash
   source ~/.sdkman/bin/sdkman-init.sh
 fi
-
-sdk install java 8.0.265.hs-adpt
-sdk install java 11.0.8.hs-adpt
-sdk install maven 3.6.3
-sdk install gradle 6.6.1
-
-sdk use java 8.0.265.hs-adpt
